8243109b11906e9136b3e72e89198d11ecadf4c6
hoang <th09062003@gmail.com>
2025-04-23 20:34:54 +0700

    Frontend

---
+++ frontend/src/app/components/browse-card/browse-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-card',
+  imports: [],
+  templateUrl: './browse-card.component.html',
+  styleUrl: './browse-card.component.css'
+})
+export class BrowseCardComponent {
+
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+platformBrowserDynamic().bootstrapModule(AppComponent)
   .catch((err) => console.error(err));
