dac74734b273c2b020580867b257cbd970b42c11
HuyNguyen777 <huynguyen6498@gmail.com>
2025-04-29 15:40:09 +0700

    play-button

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -33,7 +33,7 @@ export class LoginComponent implements OnInit {
         next: (data) => {
           this.authService.saveToken(data.access); // Lưu token vào localStorage hoặc state
           alert('Login Successful');
-          this.router.navigate(['/home']); // Chuyển hướng người dùng sau khi đăng nhập thành công
+          this.router.navigate(['/']); // Chuyển hướng người dùng sau khi đăng nhập thành công
         },
         error: (err) => {
           console.error('Login failed:', err); // In lỗi ra console để dễ dàng gỡ lỗi
